251c310302a7a888f4bfc48ad78bd27efa27020f
Ilyas Garipov <ilyasgaripov97@gmail.com>
2021-06-11 07:17:11 +0300

    Add mark function to player objects, rearrange code

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
-const Player = (name) => {
-
-  return {}
-}
-
 const GameBoard = (() => {
   const BOARD_SIZE = 3;
   let gameBoard;
 
   const init = function () {
     gameBoard = new Array(BOARD_SIZE);
-    for (let i = 0; i < BOARD_SIZE; i++) {
-      gameBoard[i] = new Array(BOARD_SIZE);
+    for (let x = 0; x < BOARD_SIZE; x++) {
+      gameBoard[x] = new Array(BOARD_SIZE);
     }
-    fillBoard('');
+    fill('');
   };
 
-  const fillBoard = function (value) {
-    for (let i = 0; i < BOARD_SIZE; i++) {
-      for (let j = 0; j < BOARD_SIZE; j++) {
-        gameBoard[i][j] = value;
+  const fill = function (value) {
+    for (let x = 0; x < BOARD_SIZE; x++) {
+      for (let y = 0; y < BOARD_SIZE; y++) {
+        gameBoard[x][y] = value;
       }
     }
   };
+
+  const update = function (x, y, value) {
+    gameBoard[x][y] = value;
+  }
 
   const display = function () {
     console.log(gameBoard);
   }
 
-  return { init, display }
+  return { init, display, update }
 })();
+
+const Player = (name) => {
+  const scores = 0;
+
+  const mark = function (x, y, marker) {
+    GameBoard.update(x, y, marker);
+  }
+
+  return { mark }
+}
+
+const Game = (() => {
+  const player1 = Player('first');
+  const player2 = Player('second');
+
+  const start = function() {
+    GameBoard.init();
+    GameBoard.display();
+  }
+
+  const detectWinner = function() {
+
+  }
+  return { start }
+})();
+
+Game.start();
